e35558931da61afffa4b85b388f266963119484d
Julio Sena <julio.657.js@gmail.com>
2016-12-26 11:42:17 -0500

    update models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def my_portfolio
-
+    @user_stocks = current_user.user_stocks
+    @user_stocks_ = current_user.stocks.select('user_stocks.id as user_stock_id ', 'stocks.*')
+    @user = current_user
   end
 end
